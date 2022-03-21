518022a7a80e9b45ad1599f8d535b9f88aaeb356
Emmanuel Guerreiro <emmanuelguerreiro98@gmail.com>
2022-03-21 18:11:54 -0300

    Cambiado .sh

--- docker-image/init.sh
+++ docker-image/init.sh
@@ -3,6 +3,8 @@
 echo "sudo docker exec -it docker-image_db_1 psql -U postgres"
 
 echo "------Conectando a postgres 127.1.1.1:5432"
-sudo docker exec -it UTN_Postgres /bin/bash
-# sudo docker exec -it UTN_Postgres psql -U postgres
+sudo docker exec -it UTN_Postgres psql -U postgres
 
+#Si se queire acceder directamente a la bash, comentar arriba 
+#y descomentar lo siguiente
+# sudo docker exec -it UTN_Postgres /bin/bash
